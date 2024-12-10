5ced53b394486b591dc55f77224f67e1eaf9f2af
Flávio Filho <flaviolaraujofilho@gmail.com>
2024-12-10 10:16:56 +0000

    Sistema OS
    O Sistema de Ordem de Serviço é um projeto desenvolvido em C# para facilitar o gerenciamento de pedidos de serviço. Ele contém funcionalidades como login de usuários, cadastro de clientes, usuários e ordens de serviço (OS), geração de relatórios e informações sobre o sistema. O controle de acesso é baseado no tipo de usuário: Administrador ou Usuário Comum .

---
+++ Sistema OS/DatabaseConnection.cs
@@ -0,0 +1,88 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace Desktp
+{
+    public class DatabaseConnection
+    {
+        private string connectionString;
+
+        public DatabaseConnection()
+        {
+            // Definindo a string de conexão
+            connectionString = "Server=localhost;Database=dbsistemaos;Uid=root;Pwd=;";
+        }
+
+        // Método para obter uma conexão MySQL
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        // Método para verificar se a conexão está funcionando
+        public bool IsConnected()
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                try
+                {
+                    conn.Open(); // Tenta abrir a conexão
+                    conn.Close(); // Fecha imediatamente após a verificação
+                    return true; // Conexão está funcionando
+                }
+                catch
+                {
+                    return false; // Conexão falhou
+                }
+            }
+        }
+
+        // Método para executar comandos SQL que retornam um único valor
+        public object ExecuteScalar(string query, params MySqlParameter[] parameters)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    return cmd.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Erro ao executar comando SQL: {ex.Message}");
+                }
+            }
+        }
+
+        // Método para executar comandos SQL que não retornam valores
+        public int ExecuteNonQuery(string query, params MySqlParameter[] parameters)
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters);
+                    }
+
+                    return cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Erro ao executar comando SQL: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ Sistema OS/TelaCadastro.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Sistema_OS
+{
+    partial class TelaCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaCadastro));
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblFone = new System.Windows.Forms.Label();
+            this.lblPerfil = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblDepartamento = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtFone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.cmbDepartamento = new System.Windows.Forms.ComboBox();
+            this.cmbPerfil = new System.Windows.Forms.ComboBox();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(36, 28);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(18, 16);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(12, 73);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(44, 16);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblFone
+            // 
+            this.lblFone.AutoSize = true;
+            this.lblFone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFone.Location = new System.Drawing.Point(12, 115);
+            this.lblFone.Name = "lblFone";
+            this.lblFone.Size = new System.Drawing.Size(38, 16);
+            this.lblFone.TabIndex = 2;
+            this.lblFone.Text = "Fone";
+            // 
+            // lblPerfil
+            // 
+            this.lblPerfil.AutoSize = true;
+            this.lblPerfil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPerfil.Location = new System.Drawing.Point(12, 233);
+            this.lblPerfil.Name = "lblPerfil";
+            this.lblPerfil.Size = new System.Drawing.Size(37, 16);
+            this.lblPerfil.TabIndex = 3;
+            this.lblPerfil.Text = "Perfil";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(12, 154);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(41, 16);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblDepartamento
+            // 
+            this.lblDepartamento.AutoSize = true;
+            this.lblDepartamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartamento.Location = new System.Drawing.Point(12, 190);
+            this.lblDepartamento.Name = "lblDepartamento";
+            this.lblDepartamento.Size = new System.Drawing.Size(93, 16);
+            this.lblDepartamento.TabIndex = 5;
+            this.lblDepartamento.Text = "Departamento";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(345, 117);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(40, 16);
+            this.lblLogin.TabIndex = 6;
+            this.lblLogin.Text = "Login";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(339, 167);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(46, 16);
+            this.lblSenha.TabIndex = 7;
+            this.lblSenha.Text = "Senha";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(60, 24);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(76, 20);
+            this.txtId.TabIndex = 8;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(62, 69);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(488, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtFone
+            // 
+            this.txtFone.Location = new System.Drawing.Point(60, 111);
+            this.txtFone.Name = "txtFone";
+            this.txtFone.Size = new System.Drawing.Size(216, 20);
+            this.txtFone.TabIndex = 8;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(60, 150);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(214, 20);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(391, 113);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(159, 20);
+            this.txtLogin.TabIndex = 8;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(391, 163);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(159, 20);
+            this.txtSenha.TabIndex = 8;
+            // 
+            // cmbDepartamento
+            // 
+            this.cmbDepartamento.FormattingEnabled = true;
+            this.cmbDepartamento.Items.AddRange(new object[] {
+            "Financeiro",
+            "RH",
+            "Vendas",
+            "Administrativo",
+            "IT"});
+            this.cmbDepartamento.Location = new System.Drawing.Point(109, 185);
+            this.cmbDepartamento.Name = "cmbDepartamento";
+            this.cmbDepartamento.Size = new System.Drawing.Size(165, 21);
+            this.cmbDepartamento.TabIndex = 9;
+            // 
+            // cmbPerfil
+            // 
+            this.cmbPerfil.FormattingEnabled = true;
+            this.cmbPerfil.Items.AddRange(new object[] {
+            "Admin",
+            "Geral",
+            "Financeiro"});
+            this.cmbPerfil.Location = new System.Drawing.Point(55, 228);
+            this.cmbPerfil.Name = "cmbPerfil";
+            this.cmbPerfil.Size = new System.Drawing.Size(121, 21);
+            this.cmbPerfil.TabIndex = 9;
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Image = global::Sistema_OS.Properties.Resources.delete;
+            this.btnDeletar.Location = new System.Drawing.Point(492, 269);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(80, 80);
+            this.btnDeletar.TabIndex = 13;
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Image = global::Sistema_OS.Properties.Resources.pesquisar;
+            this.btnPesquisar.Location = new System.Drawing.Point(347, 269);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(80, 80);
+            this.btnPesquisar.TabIndex = 12;
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Image = global::Sistema_OS.Properties.Resources.edit;
+            this.btnEditar.Location = new System.Drawing.Point(202, 269);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(80, 80);
+            this.btnEditar.TabIndex = 11;
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.Image = global::Sistema_OS.Properties.Resources.add;
+            this.btnCadastro.Location = new System.Drawing.Point(57, 269);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Size = new System.Drawing.Size(80, 80);
+            this.btnCadastro.TabIndex = 10;
+            this.btnCadastro.UseVisualStyleBackColor = true;
+            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
+            // 
+            // TelaCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnCadastro);
+            this.Controls.Add(this.cmbPerfil);
+            this.Controls.Add(this.cmbDepartamento);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtFone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblDepartamento);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPerfil);
+            this.Controls.Add(this.lblFone);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblId);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "TelaCadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Usuário";
+            this.Load += new System.EventHandler(this.TelaCadastro_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblFone;
+        private System.Windows.Forms.Label lblPerfil;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblDepartamento;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtFone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.ComboBox cmbDepartamento;
+        private System.Windows.Forms.ComboBox cmbPerfil;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Button btnDeletar;
+    }
+}
